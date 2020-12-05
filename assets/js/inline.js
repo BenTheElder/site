c4df9d2beddb67108df9d2ca445e8a0fd66ce3d5
Benjamin Elder <ben@elder.dev>
2020-12-05 03:45:53 -0800

    try using javascript for youtube

--- assets/js/inline.js
+++ assets/js/inline.js
@@ -1,6 +1,40 @@
+var ytAlreadyInjected = false;
 function playYT(s) {
-    var id = s.getAttribute("data-id");
+    var oldYTAlreadyInjected = ytAlreadyInjected
+    // inject youtube API if we haven't already
+    if (!oldYTAlreadyInjected) {
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        ytAlreadyInjected = true;
+    }
+    var videoID = s.getAttribute("data-id");
     var wrapper = s.parentElement;
-    wrapper.innerHTML = '<iframe allowfullscreen="" title="YouTube Video" src="https://www.youtube.com/embed/' + id + '?autoplay="></iframe>';
-    wrapper.childNodes[0].src += "1";
+    var videoDiv = wrapper.children[0];
+    // cleanup old chlidren
+    // TODO: this could be simpler
+    var obj = wrapper.children[1];
+    var play = wrapper.children[2];
+    obj.remove();
+    play.remove();
+    videoDiv.onclick = null;
+    videoDiv.classList = "";
+    createVid = function () {
+        player = new YT.Player(videoDiv, {
+            videoId: videoID,
+            // TODO: mobile?
+            playerVars: { 'autoplay': 1 },
+            width: "100%",
+            height: "100%",
+        });
+    }
+    if (!oldYTAlreadyInjected) {
+        window.onYouTubeIframeAPIReady = function () {
+            createVid();
+        }
+    } else {
+        // TODO: what if it's injected already but still loadinig
+        createVid();
+    }
 }
